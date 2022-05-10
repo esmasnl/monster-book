6c79753e1c81588ad9eafc3a5f62c5956ffb26ae
esmasnl <ercument1649@gmail.com>
2022-05-10 20:44:17 +0300

    3 sayfa tasarlandı

---
+++ app/src/main/java/com/essel/monsterbook/ui/accountSettings/AccountSettingsViewModel.java
@@ -0,0 +1,7 @@
+package com.essel.monsterbook.ui.accountSettings;
+
+import androidx.lifecycle.ViewModel;
+
+public class AccountSettingsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/essel/monsterbook/ui/summary/SummaryViewModel.java
@@ -0,0 +1,7 @@
+package com.essel.monsterbook.ui.summary;
+
+import androidx.lifecycle.ViewModel;
+
+public class SummaryViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
